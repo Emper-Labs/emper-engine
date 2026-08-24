c840ecf23f5125d7d740e26d821b44e2e0d66a90
phamvanbinhminh <phamvanbinhminh210@gmail.com>
2026-08-24 15:51:56 +0700

    delete: delete ontickFunc from FieldBuilder

--- include/emper/simulation/storage/Field.h
+++ include/emper/simulation/storage/Field.h
@@ -14,25 +14,15 @@ namespace emper::interfaces::backend { class IRenderer; }
 namespace emper::storage
 {
 
-// Fluent builder returned by World::registerType<T>().
-//
-// Each .field<&T::member>() call adds a SoA column to a temporary
-// TypeStorage owned by this builder. When the builder is destroyed
-// (normally at the end of the registration expression) the storage is
-// committed to the World.
 template<typename T>
 class FieldBuilder
 {
 public:
-    using CommitFn = std::function<void(std::unique_ptr<TypeStorage<T>>)>;
-    using OnTickFn = std::function<void(storage::StorageView<T>&,
-                                        float,
-                                        interfaces::backend::IRenderer*)>;
-    using RegisterOnTickFn = std::function<void(OnTickFn)>;
+    using CommitFn =
+        std::function<void(std::unique_ptr<TypeStorage<T>>)>;
 
-    explicit FieldBuilder(CommitFn fn, RegisterOnTickFn onTickFn = {})
+    explicit FieldBuilder(CommitFn fn)
         : commitFn_(std::move(fn))
-        , onTickRegisterFn_(std::move(onTickFn))
         , storage_(std::make_unique<TypeStorage<T>>())
     {
     }
@@ -42,14 +32,10 @@ public:
 
     FieldBuilder(FieldBuilder&& other) noexcept
         : commitFn_(std::move(other.commitFn_))
-        , onTickRegisterFn_(std::move(other.onTickRegisterFn_))
-        , onTickFn_(std::move(other.onTickFn_))
         , storage_(std::move(other.storage_))
         , committed_(other.committed_)
-        , tickSet_(other.tickSet_)
     {
         other.committed_ = true;
-        other.tickSet_ = true;
     }
 
     FieldBuilder& operator=(FieldBuilder&& other) noexcept
@@ -57,14 +43,12 @@ public:
         if (this != &other)
         {
             commitFn_ = std::move(other.commitFn_);
-            onTickRegisterFn_ = std::move(other.onTickRegisterFn_);
-            onTickFn_ = std::move(other.onTickFn_);
             storage_ = std::move(other.storage_);
             committed_ = other.committed_;
-            tickSet_ = other.tickSet_;
+
             other.committed_ = true;
-            other.tickSet_ = true;
         }
+
         return *this;
     }
 
@@ -80,18 +64,7 @@ public:
         return *this;
     }
 
-    // Register a per-type update/render callback.
-    //
-    // The callback is invoked twice per frame by the World:
-    //   * during the update phase: dt > 0, renderer == nullptr   -> simulation
-    //   * during the render phase: dt == 0, renderer != nullptr  -> drawing
-    template<typename Fn>
-    FieldBuilder& onTick(Fn&& fn)
-    {
-        onTickFn_ = OnTickFn(std::forward<Fn>(fn));
-        return *this;
-    }
-
+private:
     void commit()
     {
         if (!committed_ && commitFn_ && storage_)
@@ -99,20 +72,10 @@ public:
             commitFn_(std::move(storage_));
             committed_ = true;
         }
-        if (!tickSet_ && onTickRegisterFn_ && onTickFn_)
-        {
-            onTickRegisterFn_(std::move(onTickFn_));
-            tickSet_ = true;
-        }
     }
 
-private:
     CommitFn commitFn_;
-    RegisterOnTickFn onTickRegisterFn_;
-    OnTickFn onTickFn_;
     std::unique_ptr<TypeStorage<T>> storage_;
     bool committed_ = false;
-    bool tickSet_ = false;
 };
-
 } // namespace emper::storage
